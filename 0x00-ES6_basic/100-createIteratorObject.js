39c1a35eab6b78e9879f0402ad72c98116a5ecb0
BrendaWanjiru <brendawanjiru5@gmail.com>
2024-03-05 13:29:12 +0000

    Es6 basic

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,12 +1,7 @@
 export default function createIteratorObject(report) {
-  const departments = Object.values(report);
-
-  // Flatten the array of employees from all departments
-  const allEmployees = departments.flatMap(department => department);
-
-  // Create an iterator for the array of all employees
-  const iterator = allEmployees[Symbol.iterator]();
-
-  return iterator;
+  const result = [];
+  for (const idx of Object.values(report.allEmployees)) {
+    result.push(...idx);
+  }
+  return result;
 }
-
